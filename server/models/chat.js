1a7c66c0b1777257597b0aa99e67431cfc89744b
Bhavani Sri Alekhya <152136977+PBS-Alekhya@users.noreply.github.com>
2025-08-15 19:23:45 +0530

    Update chat.js

--- server/models/chat.js
+++ server/models/chat.js
@@ -1,1 +1,26 @@
+const mongoose = require('mongoose');
 
+const chatSchema = new mongoose.Schema({
+  roomId: {
+    type: String,
+    required: true,
+  },
+  sender: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  time: {
+    type: String, // use String to match your format like '10:30:25 AM'
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Chat', chatSchema);
